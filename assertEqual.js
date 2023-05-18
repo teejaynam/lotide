9296066ee32b38a4ae685c5fa9aadc3e8598f00b
TJ <teejaynam@gmail.com>
2023-05-17 20:34:49 -0400

    assertEqual first commit Lotide project

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let outcome = actual === expected ? "🟢 Assertion Passed: " + actual + " === " + expected : "🔴 Assertion Failed: " + actual + " !== " + expected;
+  console.log(outcome);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 4);
+assertEqual("poppy", "poppy");
