dab084af8a1306905602f42027582438c48758a0
TJ <teejaynam@gmail.com>
2023-05-17 21:09:41 -0400

    tail first commit

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let outcome = actual === expected ? `🟢 Assertion Passed: ${actual} === ${expected}` : `🔴 Assertion Failed: ${actual} !== ${expected}`;
+  console.log(outcome);
+};
+
+const tail = function(array) {
+  let newArray = array.slice(1,array.length);
+  return newArray;
+};
+
+//tests
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
