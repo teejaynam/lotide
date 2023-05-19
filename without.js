687045095797c01d29dbc32c6b7cadae59776f51
TJ <teejaynam@gmail.com>
2023-05-19 17:09:18 -0400

    without exercise lotide first commit

--- without.js
+++ without.js
@@ -12,10 +12,33 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-const assertArraysEqual = function(actual,expected){
-  if (eqArrays(actual,expected)){
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual,expected)) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  }else{
+  } else {
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
+
+const without = function(source, itemsToRemove) {
+  let subset = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      subset.push(source[i]);
+    }
+  }
+
+  return subset;
+};
+
+/*
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+
+console.log(words);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+*/
