dc8cc02f29cd121882d4f00fea4ad04bb93bb7de
TJ <teejaynam@gmail.com>
2023-05-19 16:17:52 -0400

    assertArraysEqual with eslint fix :)

--- without.js
+++ without.js
@@ -12,14 +12,10 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-const assertArraysEqual = function(actual,expected) {
-  if (eqArrays(actual,expected)) {
+const assertArraysEqual = function(actual,expected){
+  if (eqArrays(actual,expected)){
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  }else{
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[4,5,6]);
-assertArraysEqual([1,2,3],["1","2",3]);
+}
