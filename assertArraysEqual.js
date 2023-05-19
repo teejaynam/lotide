9c005b0e8f35b0a1a6afc01d7958a63b57f4e9be
TJ <teejaynam@gmail.com>
2023-05-19 16:16:16 -0400

    assertArraysEqual first commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected){
+  if (eqArrays(actual,expected)){
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  }else{
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[4,5,6]);
+assertArraysEqual([1,2,3],["1","2",3]);
