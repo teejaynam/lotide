36ff2e90c9f17e7dec9c6a5dce153613e6e2ed27
TJ <teejaynam@gmail.com>
2023-05-26 16:58:30 -0400

    findKey function  with three test cases - first commit

---
+++ findKey.js
@@ -0,0 +1,39 @@
+// take two variables (primitive), and compare them for strict equality
+const assertEqual = function(actual, expected) {
+  let outcome = actual === expected ? `🟢 Assertion Passed: ${actual} === ${expected}` : `🔴 Assertion Failed: ${actual} !== ${expected}`;
+  console.log(outcome);
+};
+
+const findKey = function(object, callback) {
+  for (const key of Object.keys(object)) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma");
+
+assertEqual(findKey({
+  "Toronto":     { province: "Ontario" },
+  "Scarborough": { province: "Ontario" },
+  "Montreal":    { province: "Quebec" },
+  "Vancouver":   { province: "British Columbia" },
+  "Laval":       { province: "Quebec" }
+}, x => x.province === "Quebec"),"Montreal");
+
+assertEqual(findKey({
+  "Toronto":     { province: "Ontario" },
+  "Scarborough": { province: "Ontario" },
+  "Montreal":    { province: "Quebec" },
+  "Vancouver":   { province: "British Columbia" },
+}, x => x.province === "Alberta"),undefined);
