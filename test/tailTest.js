9adbce7f1a1b678a74996b488790cda90ff8580a
TJ <teejaynam@gmail.com>
2023-06-07 19:13:47 -0400

    reconfigured tests to pass all comparisons

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,14 +5,14 @@ const tail = require('../tail')
 
 // TEST CODE
 const test1 = ["Yo Yo", "Lighthouse", "Labs"];
-
 console.log("test that tail does not modify original array after passing to tail() :",tail(test1));
 console.log("original array length (3) should pass assertEqual")
 assertEqual(test1.length, 3);
 
-console.log("test should fail, cannot shallow compare two array objects directly");
-assertEqual(tail(test1),["Lighthouse","Labs"]);
+const test2 = tail(tail(test1));
+console.log("tail(tail(test1)) saved into new variable, modified array length comparison should pass assertEqual");
+assertEqual(test2.length,1);
 
-const test2 = tail(test1);
+const test3 = tail(test1);
 console.log("modified array saved into new variable, modified array length comparison should pass assertEqual");
-assertEqual(test2.length, 2);
+assertEqual(test3.length, 2);
