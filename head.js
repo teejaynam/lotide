78b516513ad7528a873dc3672692fac73f0ded80
TJ <teejaynam@gmail.com>
2023-06-07 18:26:46 -0400

    added comments, removed test code and added export

--- head.js
+++ head.js
@@ -1,11 +1,9 @@
+// head.js
+
 const head = function(array) { //returns first element of the array
   return array[0];
 };
 
-const assertEqual = require('./assertEqual')
+// export function
 
-
-// testing
-assertEqual(head([7,8,7]), 7);
-assertEqual(head(["Hello", "World"]), "Hello");
-assertEqual(head(["Womp", "Lighthouse", "Labs"]), "Womp");
+module.exports = head;
