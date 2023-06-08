d59d747a124445931757424b761de34411494de5
TJ <teejaynam@gmail.com>
2023-06-08 17:03:47 -0400

    refactored index.js to use object short hand notation

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
-};
+module.exports = { head, tail, middle };
 
