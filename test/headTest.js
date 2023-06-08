7acfe8e870e97cba3920e031abe84e945354a980
TJ <teejaynam@gmail.com>
2023-06-07 20:53:21 -0400

    converted headTest to use mocha describe and it and chai assert

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,7 @@
 // test/headTest.js
 
-//const assertEqual = require('../assertEqual')
 const head = require('../head')
 const assert = require('chai').assert;
-
 
 // TEST CODE
 
@@ -20,16 +18,5 @@ describe("#head", () => {
   it("returns 'Hello' for ['Hello', 'World']", () => {
     assert.strictEqual(head(['Hello', 'World']), 'Hello');
   });
-  
+
 });
-
-/*
-console.log("Head of array, `7`, should pass assertEquals")
-assertEqual(head([7,8,7]), 7);
-
-console.log("Head of array, `Hello`, should pass assertEquals")
-assertEqual(head(["Hello", "World"]), "Hello");
-
-console.log("Head of array, `Womp`, should fail assertEquals")
-assertEqual(head(["Womp", "Lighthouse", "Labs"]), "Pop");
-*/
