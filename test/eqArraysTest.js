e1c912158bc6439377b0fe3c82aa1f453bb7e80c
TJ <teejaynam@gmail.com>
2023-06-10 15:20:57 -0400

    added a nested array test case for recursive functionality in eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -14,3 +14,5 @@ assertEqual(eqArrays(['medium', 'L'], ['medium', 'L']), true);
 console.log("Comparison of two unlike arrays should return false, and pass assertEquals with second comparator as false");
 assertEqual(eqArrays([4,5,6], ["4",5,6]), false);
 
+console.log("Comparison of two alike arrays of with nested arrays should pass assertEquals");
+assertEqual(eqArrays(['small', 'medium', [1,2]], ['small', 'medium', [1,2]]), true);
