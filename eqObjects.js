34256bfb30c454dc34c8b0a31ea4e2be3bfe8576
TJ <teejaynam@gmail.com>
2023-06-10 15:29:56 -0400

    removed test cases and the rewritten assertEquals functions, exported module

--- eqObjects.js
+++ eqObjects.js
@@ -1,27 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let outcome = actual === expected ?
-    `🟢 Assertion Passed: ${actual} === ${expected}` :
-    `🔴 Assertion Failed: ${actual} !== ${expected}`;
-  console.log(outcome);
-};
-
-const eqArrays = function(actual, expected) {
-  if (Array.isArray(actual) && Array.isArray(expected)) {
-    if (actual.length !== expected.length) {
-      return false;
-    }
-
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  return false;
-};
+// eqObjects.js
+// recursive function that checks deep equality between two objects
 
 const eqObjects = function(object1, object2) {
   const object1Keys = Object.keys(object1);
@@ -53,25 +31,6 @@ const eqObjects = function(object1, object2) {
   return true; //if all checks pass, return true
 };
 
-// tests
+// export function
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
-
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-const test4 = { size: "medium", color: "blue", sleeveLength: "long" };
-
-const test5 = { color: "blue", sleeveLength: "long", size: "medium" };
-
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
-assertEqual(eqObjects(longSleeveShirtObject , test4), false);
-assertEqual(eqObjects(test4,test5), true);
-
-
-const test6 = { size: ["medium","L"], color: "blue", sleeveLength: "long" };
-const test7 = { color: "blue", sleeveLength: "long", size: ["medium","L"] };
-assertEqual(eqObjects(test6,test7), true);
-
-
-
+module.exports = eqObjects;
