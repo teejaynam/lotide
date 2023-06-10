18aa3d53dcafcc8775940caa3017a75fbf5792c1
TJ <teejaynam@gmail.com>
2023-06-10 15:06:14 -0400

    refactored eqObjects to use recursion

--- eqObjects.js
+++ eqObjects.js
@@ -24,28 +24,33 @@ const eqArrays = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  const object1Keys = Object.keys(object1); // create an array of keys of the given objects
+  const object1Keys = Object.keys(object1);
   const object2Keys = Object.keys(object2);
 
-  if (object1Keys.length !== object2Keys.length) { // if they are of inequal length, return false
+  if (object1Keys.length !== object2Keys.length) { //if objects are inequal length, return false immediately
     return false;
   }
 
-  for (const key of object1Keys) { // for each key in the object key arrays, if its not included in the object 2 keys, return false
+  for (let key of object1Keys) { //if key from object1 is not included in object2, return false immediately
     if (!object2Keys.includes(key)) {
       return false;
     }
 
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // if the key value of both objects are arrays, check if eqArrays is false for the given values
-      if (!eqArrays(object1[key],object2[key])) {
+    const value1 = object1[key]; //assign values of object at key into variables in case of nested objects
+    const value2 = object2[key];
+
+    if (typeof value1 !== "object") { //if typeof isnt an object, check for equality, return false if not strictly equal
+      if (value1 !== value2) {
         return false;
-      } // an array that passes the conditions here will fail at this shallow check so we put an else if
-    } else if (object1[key] !== object2[key]) { //if they werent both arrays, we check for shallow equality of the values of the object at that key
-      return false;
+      }
+    } else { 
+      if (!eqObjects(value1, value2)) { //recursive call of eqObjects, passing our object[key] variables as arguments
+        return false;
+      }
     }
   }
 
-  return true; // if key+value conditions all go through, they are equal, and return true
+  return true; //if all checks pass, return true
 };
 
 // tests
