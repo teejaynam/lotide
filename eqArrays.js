13753c81c5700b7e90abbdbf57a51f4ed039f4b3
TJ <teejaynam@gmail.com>
2023-06-10 15:20:43 -0400

    refactored eqArrays to use recursion

--- eqArrays.js
+++ eqArrays.js
@@ -2,17 +2,20 @@
 // function that compares two arrays for strict equality, if there is at least one inequal element, it will return false
 
 const eqArrays = function(actual, expected) {
-  if (Array.isArray(actual) && Array.isArray(expected)) {
-    if (actual.length !== expected.length) {
+  if (Array.isArray(actual) && Array.isArray(expected)) { //checks if the arrays are arrays
+    if (actual.length !== expected.length) { //checks if the lengths are the same or else return false immediately
       return false;
     }
 
     for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
+      if (Array.isArray(actual[i]) && Array.isArray(expected[i])) { //iterate through the array, check for nested array
+        if (!eqArrays(actual[i], expected[i])) { //if it is a nested array, check equality by calling eqArrays again, if false, return false immediately
+          return false;
+        }
+      } else if (actual[i] !== expected[i]) {
         return false;
       }
     }
-
     return true;
   }
 
