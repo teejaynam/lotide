450d20f0391a557ef0c5b06041d5e81f05c7370b
TJ <teejaynam@gmail.com>
2023-06-10 15:48:57 -0400

    removed rewritten assertEquals codes, removed test cases, added module export

--- flatten.js
+++ flatten.js
@@ -1,27 +1,10 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual,expected) {
-  eqArrays(actual, expected)
-    ? console.log(`🟢 Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
+// flatten.js
 // function that returns a flattened version of a given array
+
 const flatten = function(source) {
   return source.flat();
 };
 
-// testing
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1,2,3,4,5,6]);
+// export function
+
+module.exports = flatten;
