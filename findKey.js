f024e2d01622ae7175c9b4207c300129d5d5482e
TJ <teejaynam@gmail.com>
2023-06-12 15:42:23 -0400

    update findKey to remove rewritten functions and test cases. Add function export

--- findKey.js
+++ findKey.js
@@ -1,8 +1,4 @@
-// take two variables (primitive), and compare them for strict equality
-const assertEqual = function(actual, expected) {
-  let outcome = actual === expected ? `🟢 Assertion Passed: ${actual} === ${expected}` : `🔴 Assertion Failed: ${actual} !== ${expected}`;
-  console.log(outcome);
-};
+// findKey.js
 
 const findKey = function(object, callback) {
   for (const key of Object.keys(object)) { //for..of loop to iterate through array of object's keys,
@@ -13,29 +9,6 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
+// export
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2),"noma");
-
-assertEqual(findKey({
-  "Toronto":     { province: "Ontario" },
-  "Scarborough": { province: "Ontario" },
-  "Montreal":    { province: "Quebec" },
-  "Vancouver":   { province: "British Columbia" },
-  "Laval":       { province: "Quebec" }
-}, x => x.province === "Quebec"),"Montreal");
-
-assertEqual(findKey({
-  "Toronto":     { province: "Ontario" },
-  "Scarborough": { province: "Ontario" },
-  "Montreal":    { province: "Quebec" },
-  "Vancouver":   { province: "British Columbia" },
-}, x => x.province === "Alberta"),undefined);
-
-assertEqual(findKey({}, x => x.province === "New Brunswick"),undefined);
+module.exports = findKey;
