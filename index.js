8b214e0d31727dce4c37a9a139287056afe4776c
TJ <teejaynam@gmail.com>
2023-06-12 15:09:20 -0400

    update index.js to include the rest of the library's exported functions

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const letterPositions = require('./letterPositions');
 const findKeyByValue = require('./findKeyByValue');
 const eqObjects = require('./eqObjects');
 const eqArrays = require('./eqArrays');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const findKey = require('./findKey');
+const countLetters = require('./countLetters');
 
-module.exports = { head, tail, middle, flatten, countOnly, letterPositions, findKeyByValue, eqObjects, eqArrays };
+
+module.exports = { head, tail, middle, flatten, countOnly, letterPositions, findKeyByValue, eqObjects, eqArrays, without, takeUntil, map, findKey, countLetters };
 
