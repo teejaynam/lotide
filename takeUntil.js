06e69bf75aec28a14d4bb087f672a14d19e5969e
TJ <teejaynam@gmail.com>
2023-06-12 15:31:11 -0400

    update takeUntil to remove rewritten functions and test cases. Also add export function

--- takeUntil.js
+++ takeUntil.js
@@ -1,24 +1,4 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-// side effect function that returns a pass if eqArrays is true or fail is returns false
-const assertArraysEqual = function(actual,expected) {
-  eqArrays(actual, expected)
-    ? console.log(`🟢 Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
+// takeUntil.js
 
 const takeUntil = function(array, callback) {
   const result = [];
@@ -34,19 +14,6 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
+//export
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(results1,[1,2,5,7,2]);
-assertArraysEqual(results2,["I've","been","to","Hollywood"]);
-
-const data3 = ["I","Am","Iron","Man","Three"];
-assertArraysEqual(takeUntil(data3, x => x === 'Three'),["I","Am","Iron","Man"]);
+module.exports = takeUntil;
