11bf12a91d6d51f10fbb569a3b8301c5b2861e77
TJ <teejaynam@gmail.com>
2023-06-12 15:17:20 -0400

    feature test file for assertObjectsEqual function with two test cases

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,11 +1,11 @@
 // test/assertObjectsEqualTest
 
-const assertObjectsEqual = require('../assertObjectsEqual')
+const assertObjectsEqual = require('../assertObjectsEqual');
 
 // TEST CODE
 const test6 = { size: ["medium","L"], color: "blue", sleeveLength: "long" };
 const test7 = { color: "blue", sleeveLength: "long", size: ["medium","L"] };
-assertObjectsEqual(test6,test7); 
+assertObjectsEqual(test6,test7);
 
 const test3 = { color: "red", size: "medium", sleeve : { sleeveLength: "long" } };
 const test4 = { size: "medium", color: "red", sleeve : { sleeveLength: "long" } };
