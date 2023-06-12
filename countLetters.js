bdf674d431e7a326436a878779f27f364fba6b48
TJ <teejaynam@gmail.com>
2023-06-12 15:46:12 -0400

    update countLetters to remove rewritten functions and test code, add function export

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let outcome = actual === expected ? `🟢 Assertion Passed: ${actual} === ${expected}` : `🔴 Assertion Failed: ${actual} !== ${expected}`;
-  console.log(outcome);
-};
+// countLetters.js
 
 const countLetters = function(sentence) {
   count = {};
@@ -20,11 +17,4 @@ const countLetters = function(sentence) {
   return count;
 };
 
-// testing
-const test = countLetters("lighthouse in the house");
-//{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
-
-assertEqual(test["i"],2);
-assertEqual(test["h"],4);
-assertEqual(test.h,3);
-assertEqual(test.e,3);
+module.exports = countLetters;
